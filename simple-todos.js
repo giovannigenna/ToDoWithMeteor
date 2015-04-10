edf75d24c7092f610fcb3329bd840e3a0c42d491
unknown <Genna@GENNA-W8.elmec.ad>
2015-04-10 11:00:51 +0200

    .

---
+++ simple-todos.js
@@ -0,0 +1,19 @@
+if (Meteor.isClient) {
+
+    Template.body.helpers({
+        tasks: [
+            { text: 'This is task 1' },
+            { text: 'This is task 2' },
+            { text: 'This is task 3' },
+            { text: 'ciao'}
+        ]
+    });
+}
+
+if (Meteor.isServer) {
+  Meteor.startup(function () {
+    // code to run on server at startup
+  });
+}
+
+
