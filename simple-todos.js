676a5f4160f15d79d007bf75ffc7b00428885e64
unknown <Genna@GENNA-W8.elmec.ad>
2015-04-10 12:15:35 +0200

    Aggiunto listener eventi

--- simple-todos.js
+++ simple-todos.js
@@ -3,7 +3,34 @@ Tasks = new Mongo.Collection("tasks");
 if (Meteor.isClient) {
 
     Template.body.helpers({
-        tasks: function() { return Tasks.find({}); }
+        tasks: function() { return Tasks.find({}, {sort:{createdAt: -1}}); }
+    });
+
+    Template.body.events({
+        'submit .new-task': function( event ) {
+            var text = event.target.text.value;
+
+            Tasks.insert({
+                text: text,
+                createdAt: new Date()
+            });
+
+            // Clear form
+            event.target.text.value = '';
+
+            // Prevent default form submit
+            return false;
+        }
+
+    });
+
+    Template.task.events({
+       'click .toggle-checked': function( event ) {
+           Tasks.update( this._id, {$set: {checked: !this.checked}} );
+       },
+       'click .delete': function( event ) {
+           Tasks.remove( this._id );
+       }
     });
 
 }
