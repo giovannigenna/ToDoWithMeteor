fe685893ebab7fab07be4f37bbc80af001f51a1d
unknown <Genna@GENNA-W8.elmec.ad>
2015-04-10 15:31:10 +0200

    Aggiunto supporto con login utente

--- simple-todos.js
+++ simple-todos.js
@@ -3,25 +3,43 @@ Tasks = new Mongo.Collection("tasks");
 if (Meteor.isClient) {
 
     Template.body.helpers({
-        tasks: function() {
-            if( Session.get('hideCompleted') ) {
-                return Tasks.find({checked: {$ne: true}}, {sort:{createdAt: -1}});
+        tasks: function () {
+            if (Session.get('hideCompleted')) {
+                return Tasks.find({
+                    checked: {
+                        $ne: true
+                    }
+                }, {
+                    sort: {
+                        createdAt: -1
+                    }
+                });
             }
 
-            return Tasks.find({}, {sort:{createdAt: -1}});
+            return Tasks.find({}, {
+                sort: {
+                    createdAt: -1
+                }
+            });
         },
-        incompleteCount: function() {
-            return Tasks.find({checked: {$ne: true}} ).count();
+        incompleteCount: function () {
+            return Tasks.find({
+                checked: {
+                    $ne: true
+                }
+            }).count();
         }
     });
 
     Template.body.events({
-        'submit .new-task': function( event ) {
+        'submit .new-task': function (event) {
             var text = event.target.text.value;
 
             Tasks.insert({
                 text: text,
-                createdAt: new Date()
+                createdAt: new Date(),
+                owner: Meteor.userId(),
+                username: Meteor.user().username
             });
 
             // Clear form
@@ -30,7 +48,7 @@ if (Meteor.isClient) {
             // Prevent default form submit
             return false;
         },
-        'change .hide-completed input': function( event ) {
+        'change .hide-completed input': function (event) {
             Session.set('hideCompleted', event.target.checked);
         }
 
@@ -38,20 +56,26 @@ if (Meteor.isClient) {
     });
 
     Template.task.events({
-       'click .toggle-checked': function( event ) {
-           Tasks.update( this._id, {$set: {checked: !this.checked}} );
-       },
-       'click .delete': function( event ) {
-           Tasks.remove( this._id );
-       }
+        'click .toggle-checked': function (event) {
+            Tasks.update(this._id, {
+                $set: {
+                    checked: !this.checked
+                }
+            });
+        },
+        'click .delete': function (event) {
+            Tasks.remove(this._id);
+        }
+    });
+
+    Accounts.ui.config({
+        passwordSignupFields: "USERNAME_ONLY"
     });
 
 }
 
 if (Meteor.isServer) {
-  Meteor.startup(function () {
-    // code to run on server at startup
-  });
+    Meteor.startup(function () {
+        // code to run on server at startup
+    });
 }
-
-
